c61f39722644859e8ae60eee112f5e7f3d47d6f3
birnbap <birnbap@gmail.com>
2015-11-18 07:32:07 +0200

    R script added content to functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+#Store the inverse matrix in the memory to speed up future calculations 
+#makeMatrix - Contractor function to a structure to be used with the cacheSolve function  
+#Input : invertible matrix 
+#Output - a list of functions  that contain globally set a head variables 
+##Usage example 
+#a<-matrix(runif(9),3)
+#c<-makeMatrix(a)
+#c1<-cacheSolve(c)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    #set /change the matrix stored in memeory
+    set <- function(y) {
+        #superassignment  is used to change the global variable 
+        x <<- y
+        m <<- NULL
+    }
+    # get the matrix
+    get <- function() x
+    #stores the inverse matrix
+    setInverse <- function(inverse) m <<- inverse
+    #returns the inverse matrix stored in memory
+    getInverse <- function() m
+    list(#set = set,
+         get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+#cacheSolve : Returns an inverse matrix from memory if pre calculated
+#else it calculates the inverse matrix , stores it an return it 
+#input : the makeMatrix output
+#Output - the inverted matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getInverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setInverse(m)
+    m
 }
